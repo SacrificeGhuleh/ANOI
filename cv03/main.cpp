a5c289ca1e4325bec7fcc515d3562ca4c5041eee
Richard Zvonek <zvonek.r@seznam.cz>
2020-04-11 10:07:29 +0200

    CV03 added CLI

--- cv03/main.cpp
+++ cv03/main.cpp
@@ -31,20 +31,20 @@ int main(int argc, char **argv) {
     std::string inputImagePath = "images/test01.png";
     std::string input2ImagePath = "images/test02.png";
     
-//    ArgParser args(argc, argv);
-//    while (args.hasNext()) {
-//      std::string opt = args.getNextOpt();
-//      if (opt == "i") {
-//        inputImagePath = args.getNextValue();
-//      }
-//      if (opt == "t") {
-//        trainImagePath = args.getNextValue();
-//      } else if (opt == "h" || opt == "help") {
-//        printUsage();
-//        return 1;
-//      } else
-//        throw std::invalid_argument("invalid argument");
-//    }
+    ArgParser args(argc, argv);
+    while (args.hasNext()) {
+      std::string opt = args.getNextOpt();
+      if (opt == "i") {
+        inputImagePath = args.getNextValue();
+      }
+      else if (opt == "t") {
+        trainImagePath = args.getNextValue();
+      } else if (opt == "h" || opt == "help") {
+        printUsage();
+        return 1;
+      } else
+        throw std::invalid_argument("invalid argument");
+    }
     
     
     Classifier trainingClassifier(trainImagePath);
@@ -52,46 +52,49 @@ int main(int argc, char **argv) {
     trainingClassifier.classify();
     trainingClassifier.update();
     trainingClassifier.print();
-    trainingClassifier.prepareVisualization();
-    trainingClassifier.show();
-    
+  
     ObjectClass squareObjectClass("Square", Color::Red);
     ObjectClass rectangleObjectClass("Rectangle", Color::Green);
     ObjectClass starObjectClass("Star", Color::Blue);
-    
+  
     // Squares
     squareObjectClass.addObject(trainingClassifier.getObjects().at(0));
     squareObjectClass.addObject(trainingClassifier.getObjects().at(1));
     squareObjectClass.addObject(trainingClassifier.getObjects().at(2));
     squareObjectClass.addObject(trainingClassifier.getObjects().at(3));
-    
+  
     // Stars
     starObjectClass.addObject(trainingClassifier.getObjects().at(4));
     starObjectClass.addObject(trainingClassifier.getObjects().at(5));
     starObjectClass.addObject(trainingClassifier.getObjects().at(6));
     starObjectClass.addObject(trainingClassifier.getObjects().at(7));
-    
+  
     // Rectangles
     rectangleObjectClass.addObject(trainingClassifier.getObjects().at(8));
     rectangleObjectClass.addObject(trainingClassifier.getObjects().at(9));
     rectangleObjectClass.addObject(trainingClassifier.getObjects().at(10));
     rectangleObjectClass.addObject(trainingClassifier.getObjects().at(11));
-    
+  
     squareObjectClass.printEtalonVector();
     starObjectClass.printEtalonVector();
     rectangleObjectClass.printEtalonVector();
-    
+  
     std::vector<ObjectClass *> objectClasses;
     objectClasses.emplace_back(&squareObjectClass);
     objectClasses.emplace_back(&starObjectClass);
     objectClasses.emplace_back(&rectangleObjectClass);
+    
+    trainingClassifier.prepareVisualization();
+    trainingClassifier.show();
+    
+
     
     {
       Classifier classifier(inputImagePath);
       classifier.preprocess();
       classifier.classify();
       classifier.update();
-      classifier.print();
+      //classifier.print();
       classifier.assignObjects(objectClasses);
       classifier.prepareVisualization();
       classifier.show();
@@ -102,7 +105,7 @@ int main(int argc, char **argv) {
       classifier.preprocess();
       classifier.classify();
       classifier.update();
-      classifier.print();
+      //classifier.print();
       classifier.assignObjects(objectClasses);
       classifier.prepareVisualization();
       classifier.show();
